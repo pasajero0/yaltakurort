5a5fbbd657972234a3fcace609fa856a73757ed8
Leonid <leomalinovsky@gmail.com>
2019-04-21 01:53:24 +0300

    ScrollToTop btn added, new mob contact menu added

--- src/js/mainSettings.js
+++ src/js/mainSettings.js
@@ -5,6 +5,16 @@ $('.header__location').click(function () {
   $('.header__locationMenuBtn').toggleClass('header__locationMenuBtn_isVisible');
 });
 
+// mobmenu is visible
+
+$('.header__showCloseBtn').click(function () {
+  if ($('.header__locationMenuBtn').hasClass('header__locationMenuBtn_isVisible')) {
+    $('.header__locationMenuBtn').removeClass('header__locationMenuBtn_isVisible');
+  }
+  $('.header__showCloseBtn').toggleClass('isOpen');
+  $('.header').toggleClass('header__isVisible');
+});
+
 // anchor smooth scroll
 
 $('.intro__button').click( function (event) {
@@ -12,4 +22,18 @@ $('.intro__button').click( function (event) {
   let id  = $(this).attr('href');
   let top = $(id).offset().top-100;
   $('body,html').animate({scrollTop: top}, 1500);
+});
+
+
+$(window).scroll(function(){
+  const scrollTopValue = $(this).scrollTop();
+	if (scrollTopValue >= 1000 ) {
+    $('.scrollToTop').css('right', '5%');
+	} else {
+    $('.scrollToTop').css('right', '-100px');
+	}
+});
+
+$('.scrollToTop').click(function () {
+  $('body,html').animate({scrollTop: 0}, 1500);
 });
